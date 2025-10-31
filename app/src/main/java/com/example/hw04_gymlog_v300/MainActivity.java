071517f5de7de3108aa53e2b02da9c80492f7dc8
Jesus Lopez-Martinez <lopezjesus.personal@gmail.com>
2025-10-30 21:33:20 -0700

    end of video 3 Database finished.

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
 
-    private static final String TAG = "DAC GYMLOG";
+    public static final String TAG = "DAC GYMLOG";
     String mExercise = "";
     double mWeight = 0.0;
     int mReps = 0;
